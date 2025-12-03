9e37d0cd6c2147b5809c73a81eb60d9391147d2d
Conchita Fraguas <cfraguasbringas@gmail.com>
2025-12-03 11:21:53 +0100

    exercise 18.9

--- R/functions.R
+++ R/functions.R
@@ -9,10 +9,11 @@ create_table_descriptive_stats <-
   function(data) {
     data |>
       dplyr::group_by(metabolite) |>
-      dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+      dplyr::summarise(dplyr::across(value, list(mean = mean, median = median, sd = sd, iqr = IQR))) |>
       dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), \(x) round(x, digits = 2))) |>
-      dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
-      dplyr::select(Metabolite = metabolite, "Mean SD" = "MeanSD")
+      dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})"))  |>
+      dplyr::mutate(MedianIQR = glue::glue("{value_median} ({value_iqr})")) |>
+      dplyr::select(Metabolite = metabolite, "Mean SD" = MeanSD, "Median IQR" = MedianIQR)
   }
 
 #' create_plot_distributions
