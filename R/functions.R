e60301a130e62a0c5931c02b987dbde246b02e05
Conchita Fraguas <cfraguasbringas@gmail.com>
2025-12-03 10:05:07 +0100

    created a new function

---
+++ R/functions.R
@@ -0,0 +1,15 @@
+#' create_table_descriptive_stats
+#'
+#' @param data
+#'
+#' @returns summarised mean + SD rounded metabolite data values
+
+create_table_descriptive_stats <-
+  function(data) {
+    data |>
+      dplyr::group_by(metabolite) |>
+      dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+      dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), \(x) round(x, digits = 2))) |>
+      dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
+      dplyr::select(Metabolite = metabolite, "Mean SD" = "MeanSD")
+  }
